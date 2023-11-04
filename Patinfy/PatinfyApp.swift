36b696ea54d12da6af3fb89e3321fed4fa13f436
Robert Alejandro Ionita Maglan <robert.alex20003@gmail.com>
2023-11-04 16:41:28 +0100

    Initial Commit

---
+++ Patinfy/PatinfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatinfyApp.swift
+//  Patinfy
+//
+//  Created by Robert Alejandro Ionita Maglan on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PatinfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
