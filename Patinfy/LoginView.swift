89fbbd414c54f1f518534bd44f7bf7d2fdf7538c
Robert Alejandro Ionita Maglan <robert.alex20003@gmail.com>
2023-11-04 17:54:29 +0100

    Login View

---
+++ Patinfy/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Patinfy
+//
+//  Created by Robert Alejandro Ionita Maglan on 4/11/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ Patinfy/Persistence.swift
@@ -0,0 +1,18 @@
+//
+//  Persistence.swift
+//  Patinfy
+//
+//  Created by Robert Alejandro Ionita Maglan on 4/11/23.
+//
+
+import SwiftUI
+
+struct Persistence: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Persistence()
+}
