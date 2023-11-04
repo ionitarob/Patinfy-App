b1164ba973e0539a02a7ca16a3485292aa596add
Robert Alejandro Ionita Maglan <robert.alex20003@gmail.com>
2023-11-04 19:42:30 +0100

    update

---
+++ Patinfy/LoginViewModel/Credentials.swift
@@ -0,0 +1,8 @@
+//
+//  Credentials.swift
+//  Patinfy
+//
+//  Created by Robert Alejandro Ionita Maglan on 4/11/23.
+//
+
+import Foundation
---
+++ Patinfy/Models/Authentification.swift
@@ -0,0 +1,8 @@
+//
+//  Authentification.swift
+//  Patinfy
+//
+//  Created by Robert Alejandro Ionita Maglan on 4/11/23.
+//
+
+import Foundation
