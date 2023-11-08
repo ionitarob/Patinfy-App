5f9088fa3092798f5ce07075c75d8646db9511a1
Robert Alejandro Ionita Maglan <robert.alex20003@gmail.com>
2023-11-08 11:36:11 +0100

    P1 v1.0

---
+++ SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  Patinfy
+//
+//  Created by Robert Alejandro Ionita Maglan on 6/11/23.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
